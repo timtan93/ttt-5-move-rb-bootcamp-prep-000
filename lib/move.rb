74be64c340e5076b52ea8dde424d3abc4951567e
Timothy Tan <timtan93@gmail.com>
2018-11-19 13:28:25 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,7 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(index)
+  puts "Welcome to Tic Tac Toe! Where would you like to go?"
+  gets (move)
+  move.to_i
